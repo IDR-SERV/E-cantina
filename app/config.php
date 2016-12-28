c5def751ad632b5a5174eecb611002199334d022
jemel-PC\jemel <jemelpastor@gmail.com>
2016-12-27 21:32:08 -0300

    Inicializacion de rutas de css, js, fonts e imagenes

---
+++ app/config.php
@@ -0,0 +1,19 @@
+<?php
+class Config{
+    public function callCss(){
+        return "../resources/views/template/dist/css/";
+    }
+
+    public function callJs(){
+        return "../resources/views/template/dist/js/";
+    }
+
+    public function callImg(){
+        return "../resources/views/template/dist/img/";
+    }
+
+    public function callFont(){
+        return "../resources/views/template/dist/fonts/";
+    }
+}
+?>
