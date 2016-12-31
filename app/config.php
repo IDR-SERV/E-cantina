4a689aaaeb1e4018e41cb07dc0a0814123b7b2e7
jemel-PC\jemel <jemelpastor@gmail.com>
2016-12-31 16:30:56 -0300

    estilos solucionados

--- app/config.php
+++ app/config.php
@@ -3,23 +3,23 @@ namespace ecantina;
 
 class Config{
     public static function callCss(){
-        return "template/dist/css/";
+        return "css/";
     }
 
     public static function callJs(){
-        return "template/dist/js/";
+        return "js/";
     }
 
     public static function callImg(){
-        return "template/dist/img/";
+        return "img/";
     }
 
     public static function callFont(){
-        return "template/dist/fonts/";
+        return "fonts/";
     }
 
     public static function callPlugins(){
-        return "template/plugins/";
+        return "plugins/";
     }
 }
 ?>
